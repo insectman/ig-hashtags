2015869ea098c469a682afd888137f4538a55da1
insectman <insectman@yandex.ru>
2020-12-22 03:18:53 +0300

    fixes

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ const express = require("express");
 
 const app = express();
 
-const server = app.listen(process.env.PORT || 8080, async () => {
+const server = app.listen(process.env.PORT || 8080);
+
+app.post('/', async (req, res) => {
     const port = server.address().port;
     console.log("App now running on port", port);
 
     const { tag, limit = 2 } = req.body;
 
-      console.log({ tag, limit });
+    console.log({ tag, limit });
 
-      const { scrapeTag } = require('./scrapeIG.js');
-      let hashTagData
+    const { scrapeTag } = require('./scrapeIG.js');
+    let hashTagData
 
-      try {
-        hashTagData = await scrapeTag(tag, limit);
-        console.log({hashTagData})
-        return res.status(200).json({hashTagData});
-      } catch( error ) {
-        console.log(error);
-        return res.status(500).json({success: false});
-      }
+    try {
+      hashTagData = await scrapeTag(tag, limit);
+      console.log({hashTagData})
+      return res.status(200).json({hashTagData});
+    } catch( error ) {
+      console.log(error);
+      return res.status(500).json({success: false});
+    }
 });
